3b9038fe558cd53383dc589e6eac23a659e7095b
jithamolns <jithamolns@gmail.com>
2023-07-13 16:50:58 +0100

    Completed adding task into array function

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
+
 var app = angular.module("todoApplication", []);
+
 app.controller("appController", function($scope){
     $scope.heading = 'My To Do List';
-  
+    $scope.error = "";  
     $scope.taskLists = []; 
     $scope.task_name = "";
 
     //Add task function
-    $scope.addTask = function(task_name){                              
-        $scope.taskLists.push(
-            {
-                taskName: task_name,
-                completed: false
-            }
-        );       
-        $scope.task_name = "";       
+    $scope.addTask = function(task_name){   
+        if(task_name == ''){
+            $scope.error = "Please enter a task";                        
+        }else{                           
+            $scope.taskLists.push(
+                {
+                    taskName: task_name,
+                    completed: false
+                }
+            );       
+            $scope.error = "";   
+            $scope.task_name = "";       
+        }
         
     }
 
