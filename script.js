7dcbdae344100f77378c1dcc71709a02a1cb8b80
jithamolns <jithamolns@gmail.com>
2023-07-13 16:49:31 +0100

    Adding task into array function done

--- script.js
+++ script.js
@@ -5,4 +5,16 @@ app.controller("appController", function($scope){
     $scope.taskLists = []; 
     $scope.task_name = "";
 
+    //Add task function
+    $scope.addTask = function(task_name){                              
+        $scope.taskLists.push(
+            {
+                taskName: task_name,
+                completed: false
+            }
+        );       
+        $scope.task_name = "";       
+        
+    }
+
  });
