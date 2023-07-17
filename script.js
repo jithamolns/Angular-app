53bcd84a33d23949db77059b47ca8fa9a3824eb6
jithamolns <jithamolns@gmail.com>
2023-07-17 10:21:04 +0100

    Created Session variables

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
 
 var app = angular.module("todoApplication", []);
 
+
 app.controller("appController", function($scope){
+
+    
+    // $scope.save = function(){
+        // sessionStorage.setItem('name', 'task');
+    // }
+
+   
+
+    $scope.removeSession = function(){
+        sessionStorage.removeItem('key');
+    }
+
+    $scope.clearAll = function(){
+        sessionStorage.clear();
+    }
+    
+    // $scope.getSession = function(user, tasks){
+    //     return sessionStorage.getItem(user, tasks);
+    // }
+    
+
     $scope.heading = 'My To Do List';
     $scope.error = "";  
     $scope.taskLists = []; 
     $scope.task_name = "";
-
+    $scope.username = ""
+    $scope.str = "";
+ 
     //Add task function
-    $scope.addTask = function(task_name){   
+    $scope.addTask = function(task_name, username){   
+
         if(task_name == ''){
             $scope.error = "Please enter a task";                        
         }else{                           
@@ -18,14 +43,22 @@ app.controller("appController", function($scope){
                     completed: false
                 }
             );       
+
+            $scope.jsonObj = JSON.stringify($scope.taskLists);
+
+            sessionStorage.setItem(username, $scope.jsonObj);
             $scope.error = "";   
-            $scope.task_name = "";       
+            $scope.task_name = "";   
+            $scope.str = sessionStorage.getItem(username);
+            $scope.taskObj = JSON.parse($scope.str);            
         }        
     }
     // Delete Task
     $scope.deleteTask = function(i){
         $scope.taskLists.splice(i, 1);
     }
+
+
 
     // Change status
     $scope.taskStatus = function(i){
